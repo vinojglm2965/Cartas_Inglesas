3f2aeb9d45e32b9031745571892817b72f6ad660
vinojglm2965 <lorantmarquina>
2020-05-14 17:56:18 -0400

    save

--- sos.js
+++ sos.js
@@ -4,12 +4,22 @@ let numbers = ['A','2','3', '4', '5', '6', '7','8','9','10','J','Q','K',];
 
 let simbols = ['♠','♣','♥','♦',];
 
-let numIndex = Math.floor(Math.random() * numbers.length);
-let simIndex = Math.floor(Math.random() * simbols.length);
+var numIndex = Math.floor(Math.random() * numbers.length);
+var simIndex = Math.floor(Math.random() * simbols.length);
 
 document.querySelector('.number').innerHTML = numbers[numIndex];
-document.querySelector('#one').innerHTML = simbols[simIndex];
-document.querySelector('#two').innerHTML = simbols[simIndex];
+document.querySelector('.sim').innerHTML = simbols[simIndex];
+document.querySelector('.simbols').innerHTML = simbols[simIndex];
+
+var one = document.getElementsByClassName('number');
+var two = document.getElementsByClassName('sim');
+var three = document.getElementsByClassName('simbols');
 
 console.log (numbers[numIndex]);
 console.log (simbols[simIndex]);
+
+ if (simbols[simIndex] == '♥' || simbols[simIndex] == '♦'){
+    one[0].style.color = 'red';
+    two[0].style.color = 'red';
+    three[0].style.color = 'red';
+}
